672b6c7e3ac4738854a438a4b3cd4a2644bdaea8
Anders Voldsund <andersvoldsund@gmail.com>
2015-10-08 22:35:32 +0200

    Working solution for task poission_2d_dirichlet

--- poisson_2d_dirichlet.m
+++ poisson_2d_dirichlet.m
@@ -1,10 +1,12 @@
-function [A] = poisson_2d_dirichlet(f, nr_of_mesh_nodes)
+function u = poisson_2d_dirichlet(f, nr_of_mesh_nodes)
 %
 
+% Get the triangulation elements, the vertices and edges
 [p, tri, edge] = getDisk(nr_of_mesh_nodes);
 A = zeros(nr_of_mesh_nodes);
+b = zeros(nr_of_mesh_nodes, 1);
 
-
+% Populate the A-matrix.
 % Iterate over all elements, find the points of the element, and find the
 % basis functions. The basis functions are linear functions and of the form
 % ax + by + c. We find a, b and c in the foor loop.
@@ -22,13 +24,42 @@ for k = 1:length(tri)
     phi3_scalars = K\b3;
     C = [phi1_scalars, phi2_scalars, phi3_scalars];
     grad = C(1:2, :);
+    
     for i = 1:3
+        % For the b-vector
+        g = @(x) C(1,i) * x(1) + C(2,i) * x(2) + C(3,i);
+        h = @(x) f(x) * g(x);
+        b(indices(i)) = b(indices(i)) + quadrature2D(p1, p2, p3, 4, h);
+        
         for j = 1:3
+        % For the A-matrix
             g = @(x) dot(grad(:, i), grad(:, j));
             A(indices(i), indices(j)) = A(indices(i), indices(j)) + quadrature2D(p1, p2, p3, 4, g);
         end
     end
+    
 end
 
+% Implementing Dirichlet boundary conditions
+% Setting all columns and rows with an index on the edge to 0. Setting the
+% diagonal elements to 1 for the zero rows.
+A(:, edge(:,1)) = 0;
+A(edge(:,1), :) = 0;
+A(edge(:,1), edge(:,1)) = eye(length(edge));
+b(edge(:,1)) = 0;
+
+u = A\b;
+
+% Plot of u
+xlin = linspace(-1, 1, nr_of_mesh_nodes);
+ylin = linspace(-1, 1, nr_of_mesh_nodes);
+[X, Y] = meshgrid(xlin, ylin);
+f = scatteredInterpolant(p(:,1), p(:,2), u);
+
+U = f(X,Y);
+U(X.^2 + Y.^2 > 1) = 0;
+
+figure
+mesh(X,Y,U)
 
 end
