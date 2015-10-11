3609c3b2ba9e3e98614a5878118ef0c59588bde6
Anders Voldsund <andersvoldsund@gmail.com>
2015-10-11 14:09:37 +0200

    Fixed the quadline function, and got poisson_2d_mixed to work-

--- poisson_2d_mixed_boundaries.m
+++ poisson_2d_mixed_boundaries.m
@@ -1,4 +1,4 @@
-function [A, b] = poisson_2d_mixed_boundaries(f, nr_of_mesh_nodes)
+function b = poisson_2d_mixed_boundaries(f, nr_of_mesh_nodes)
 
 
 % Get the triangulation elements, the vertices and edges
@@ -12,7 +12,7 @@ function [A, b] = poisson_2d_mixed_boundaries(f, nr_of_mesh_nodes)
 % top half of the disk.
 all_edge_points = p(edge(:, 1),:);
 sigma_N = edge(all_edge_points(:, 2) > 0, :);
-
+sigma_D = setdiff(edge, sigma_N, 'rows');
 g = @(x) 4 * pi;
 
 % Alter the b-vector because of the new term from the Neumann boundary
@@ -22,10 +22,46 @@ for k = 1:length(sigma_N)
     edge_point_2 = sigma_N(k, 2);
     line_start = p(edge_point_1, :);
     line_end = p(edge_point_2, :);
-    h = @(x) f(x) * g(x);
-    quadLine2D(line_start, line_end, 4, h)
-    b(edge_point_1) = b(edge_point_1) + quadLine2D(line_start, line_end, 4, h);
+    
+    K = [line_start(1) line_start(2); line_end(1) line_end(2)];
+    d_1 = [1;0]; d_2 = [0;1];
+    scalars_1 = K\d_1;
+    scalars_2 = K\d_2;
+    
+    phi_1 = @(x) scalars_1(1) * x(1) + scalars_1(2) * x(2);
+    phi_2 = @(x) scalars_2(1) * x(1) + scalars_2(2) * x(2);
+    h_1 = @(x) phi_1(x) * g(x);
+    h_2 = @(x) phi_2(x) * g(x);
+    
+    b(edge_point_1) = b(edge_point_1) + quadLine2D(line_start, line_end, 4, h_1);
+    b(edge_point_2) = b(edge_point_2) + quadLine2D(line_end, line_start, 4, h_2);
 end
 
+% Changing the A-matrix to correspond with boundary conditions
+edge_indices_with_dirchlet = union(sigma_D(:,1), sigma_D(:,2));
+A(:, edge_indices_with_dirchlet) = 0;
+A(edge_indices_with_dirchlet, :) = 0;
+A(edge_indices_with_dirchlet, edge_indices_with_dirchlet) = eye(length(edge_indices_with_dirchlet));
+b(edge_indices_with_dirchlet) = 0;
+u = A\b;
+
+trimesh(tri, p(:,1), p(:,2), u)
+str = sprintf('Numerical approximation using %d points', nr_of_mesh_nodes);
+title(str)
+
+u_analytical = zeros(nr_of_mesh_nodes, 1);
+k = @(x) sin(2 * pi * (x(1)^2 + x(2)^2));
+for i = 1 : nr_of_mesh_nodes
+    u_analytical(i) = k(p(i,:));
+end
+
+error = abs(u - u_analytical);
+max_error = max(error);
+% Plot of the error
+figure
+trimesh(tri, p(:,1), p(:,2), error)
+colorbar
+str = sprintf('Error plot. Max error: %f ', max_error);
+title(str)
 
 end
--- quadLine2D.m
+++ quadLine2D.m
@@ -2,12 +2,9 @@ function I = quadLine2D(a, b, Nq, g)
 % quadLine2D calculates the integral of the function g on the line segment
 % going from the starting point a to the end point b. Nq is the number of
 % integration points.
-[X, W] = gauss_quadrature_1D(Nq);
 
-I = 0;
-
-for i = 1:Nq
-    I = I + norm(b - a) * W(i) * g(a + (b-a) * X(i));
-end
+x = @(s) a + s * (b - a);
+h = @(s) g(x(s));
+I = norm(b - a) * quadrature1D(0, 1, Nq, h);
 
 end
